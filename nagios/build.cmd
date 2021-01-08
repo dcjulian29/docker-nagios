aeec85f665c259db387a1a9408c8329858ee5ec4
Julian Easterling <julian@julianscorner.com>
2021-01-07 19:27:49 -0500

    Fixed my "docker push" to push all tags with images

--- nagios/build.cmd
+++ nagios/build.cmd
@@ -1,4 +1,3 @@
 docker build -t dcjulian29/nagios:4.4.6 .
-docker push dcjulian29/nagios
 docker tag dcjulian29/nagios:4.4.6 dcjulian29/nagios:latest
-docker push dcjulian29/nagios
+docker push dcjulian29/nagios --all-tags
