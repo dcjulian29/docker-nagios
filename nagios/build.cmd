d85afa305e4970084483d3f641d050af0bbb66f2
Julian Easterling <julian@julianscorner.com>
2021-06-07 15:04:43 -0400

    Updated nagios docker image file to have version set separate and do a pip3 install and not a pip install. (The Debian image I'm currently using has both python3 and python2 installed.

--- nagios/build.cmd
+++ nagios/build.cmd
@@ -1,3 +1,8 @@
-docker build -t dcjulian29/nagios:4.4.6 .
-docker tag dcjulian29/nagios:4.4.6 dcjulian29/nagios:latest
+@echo off
+setlocal
+set VERSION=4.4.6
+
+docker build --pull --no-cache --progress plain -t dcjulian29/nagios:%VERSION% .
+docker tag dcjulian29/nagios:%VERSION%  dcjulian29/nagios:latest
+
 docker push dcjulian29/nagios --all-tags
