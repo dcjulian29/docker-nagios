c30991f0c681ca898bd7ae44008e950384612d47
Julian Easterling <julian@julianscorner.com>
2021-09-30 15:43:35 -0400

    Added Nagios Plugins by version number as well.

--- nagios/build.cmd
+++ nagios/build.cmd
@@ -2,15 +2,17 @@
 setlocal
 set NAGIOSVERSION=4.4.6
 set NPREVERSION=4.0.3
-
+set PLUGINVERSION=2.3.3
 
 echo --------------------------------------------------------------------------
 echo Nagios Version: %NAGIOSVERSION%
 echo NPRE Version: %NPREVERSION%
+echo Plugins Version: %PLUGINVERSION%
 echo --------------------------------------------------------------------------
 
 docker build --pull --progress plain -t dcjulian29/nagios:%NAGIOSVERSION% . ^
-  --build-arg NAGIOS_VERSION=%NAGIOSVERSION% --build-arg NPRE_VERSON=%NPREVERSON%
+  --build-arg NAGIOS_VERSION=%NAGIOSVERSION% --build-arg NPRE_VERSON=%NPREVERSON% ^
+  --build-arg PLUGIN_VERSION=%PLUGINVERSION%
 
 docker tag dcjulian29/nagios:%NAGIOSVERSION%  dcjulian29/nagios:latest
 
