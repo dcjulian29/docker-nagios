d41541a14e035089d6af52d5b342933c3689f021
Julian Easterling <julian@julianscorner.com>
2021-09-30 08:17:03 -0400

    Added a little more logic and build arguments so I don't have to update both files just for a version change.

--- nagios/build.cmd
+++ nagios/build.cmd
@@ -1,8 +1,21 @@
 @echo off
 setlocal
-set VERSION=4.4.6
+set NAGIOSVERSION=4.4.6
+set NPREVERSION=4.0.3
 
-docker build --pull --no-cache --progress plain -t dcjulian29/nagios:%VERSION% .
-docker tag dcjulian29/nagios:%VERSION%  dcjulian29/nagios:latest
+
+echo --------------------------------------------------------------------------
+echo Nagios Version: %NAGIOSVERSION%
+echo NPRE Version: %NPREVERSION%
+echo --------------------------------------------------------------------------
+
+docker build --pull --progress plain -t dcjulian29/nagios:%NAGIOSVERSION% . ^
+  --build-arg NAGIOS_VERSION=%NAGIOSVERSION% --build-arg NPRE_VERSON=%NPREVERSON%
+
+docker tag dcjulian29/nagios:%NAGIOSVERSION%  dcjulian29/nagios:latest
+
+if "%1" == "" GOTO :EOF
+
+echo --------------------------------------------------------------------------
 
 docker push dcjulian29/nagios --all-tags
