83cc3318e21f5b428f10b2eebda8fbd91d09e7da
Julian Easterling <julian@julianscorner.com>
2021-10-03 10:52:08 -0400

    Added some additional logic to create directories if missing and the set correct permissions on the nagios directories that are mounted.

--- nagios/docker-entrypoint.sh
+++ nagios/docker-entrypoint.sh
@@ -6,17 +6,45 @@ case $@ in
     ;;
 
   validate)
-    /opt/nagios/bin/nagios -v /opt/nagios/etc/nagios.cfg
+    /usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
     ;;
 
   *)
-    /opt/nagios/bin/nagios -v /opt/nagios/etc/nagios.cfg
+    if [ ! -d /usr/local/nagios/var/archives ]; then
+      mkdir /usr/local/nagios/var/archives
+    fi
+
+    if [ ! -d /usr/local/nagios/var/rw ]; then
+      mkdir /usr/local/nagios/var/rw
+    fi
+
+    if [ ! -d /usr/local/nagios/var/spool ]; then
+      mkdir /usr/local/nagios/var/spool
+    fi
+
+    if [ ! -d /usr/local/nagios/var/spool/checkresults ]; then
+      mkdir /usr/local/nagios/var/spool/checkresults
+    fi
+
+    if [ ! -d /usr/local/nagios/var/apache2 ]; then
+      mkdir /usr/local/nagios/var/apache2
+    fi
+
+    chown -R nagios:nagios /usr/local/nagios/var
+    chown -R nagios:www-data /usr/local/nagios/var/apache2
+
+    /usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
 
     if [ $? -eq 0 ]; then
-        htpasswd -b /opt/nagios/etc/htpasswd.users nagiosadmin $NAGIOS_PASSWORD
-        apachectl -k start
-        /opt/nagios/bin/nagios -d /opt/nagios/etc/nagios.cfg
-        tail -f /opt/nagios/var/nagios.log
+      if [ ! -d /usr/local/nagios/etc/htpasswd.users ]; then
+        touch /usr/local/nagios/etc/htpasswd.users
+        chown nagios:www-data /usr/local/nagios/etc/htpasswd.users
+      fi
+
+      htpasswd -bB /usr/local/nagios/etc/htpasswd.users nagiosadmin $NAGIOS_PASSWORD
+      apachectl -k start
+      /usr/local/nagios/bin/nagios -d /usr/local/nagios/etc/nagios.cfg
+      tail -f /usr/local/nagios/var/nagios.log
     fi
     ;;
 esac
