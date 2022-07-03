a52a9010f94f5aa9639f98dc3b4c7d6a6aecfa2a
Julian Easterling <julian@julianscorner.com>
2022-07-02 23:38:54 -0400

    Updated build script to use files for version variables.

--- build.cmd
+++ build.cmd
@@ -1,8 +1,11 @@
 @echo off
 setlocal
-set NAGIOSVERSION=4.4.7
-set NPREVERSION=4.0.3
-set PLUGINVERSION=2.3.3
+
+pushd %~dp0
+
+for /f "delims=" %%x in (VERSION) do set NAGIOSVERSION=%%x
+for /f "delims=" %%x in (VERSION_NPRE) do set NPREVERSION=%%x
+for /f "delims=" %%x in (VERSION_PLUGIN) do set PLUGINVERSION=%%x
 
 echo --------------------------------------------------------------------------
 echo Nagios Version: %NAGIOSVERSION%
@@ -14,10 +17,8 @@ docker build --pull --progress plain -t dcjulian29/nagios:%NAGIOSVERSION% . ^
   --build-arg NAGIOS_VERSION=%NAGIOSVERSION% --build-arg NPRE_VERSON=%NPREVERSON% ^
   --build-arg PLUGIN_VERSION=%PLUGINVERSION%
 
+if %errorlevel% neq 0 popd;exit /b %errorlevel%
+
+popd
+
 docker tag dcjulian29/nagios:%NAGIOSVERSION%  dcjulian29/nagios:latest
-
-if "%1" == "" GOTO :EOF
-
-echo --------------------------------------------------------------------------
-
-docker push dcjulian29/nagios --all-tags
