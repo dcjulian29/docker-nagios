77cb221e903b7d5787c840af0c63fd26881b38a5
Julian Easterling <julian@julianscorner.com>
2022-10-29 14:27:41 -0400

    Updated debug script to just docker-compose if not already running.

--- debug.cmd
+++ debug.cmd
@@ -6,13 +6,7 @@ set CONTAINER_NAME="docker-nagios-nagios-1"
 for /f "delims=" %%x in ('docker.exe ps -qa -f "name=%CONTAINER_NAME%"') do set CONTAINER_EXIST=%%x
 
 if "%CONTAINER_EXIST%"=="" (
-    docker run -d --name %CONTAINER_NAME% ^
-      -p "8080:80" ^
-      -e "TZ=America/New_York" ^
-      -e "NAGIOS_PASSWORD=P@ssw0rd!" ^
-      -v "%PWD%/../nagios:/usr/local/nagios/etc" ^
-      -v "%PWD%/.docker/var:/usr/local/nagios/var" ^
-      dcjulian29/nagios
+  docker compose up
 ) else (
   for /f "delims=" %%x in ('docker.exe ps -q -f "name=%CONTAINER_NAME%"') do set CONTAINER_RUNNING=%%x
 
