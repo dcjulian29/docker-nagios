d8ceba6b6ff3dc1d96e5bf3912fa367b57e71e40
Julian Easterling <julian@julianscorner.com>
2022-10-29 22:45:56 -0400

    NPRE should be NRPE

--- build.cmd
+++ build.cmd
@@ -4,17 +4,17 @@ setlocal
 pushd %~dp0
 
 for /f "delims=" %%x in (VERSION) do set NAGIOSVERSION=%%x
-for /f "delims=" %%x in (VERSION_NPRE) do set NPREVERSION=%%x
+for /f "delims=" %%x in (VERSION_NRPE) do set NRPEVERSION=%%x
 for /f "delims=" %%x in (VERSION_PLUGIN) do set PLUGINVERSION=%%x
 
 echo --------------------------------------------------------------------------
 echo Nagios Version: %NAGIOSVERSION%
-echo NPRE Version: %NPREVERSION%
+echo NRPE Version: %NRPEVERSION%
 echo Plugins Version: %PLUGINVERSION%
 echo --------------------------------------------------------------------------
 
 docker build --progress plain -t dcjulian29/nagios:%NAGIOSVERSION% . ^
-  --build-arg NAGIOS_VERSION=%NAGIOSVERSION% --build-arg NPRE_VERSON=%NPREVERSON% ^
+  --build-arg NAGIOS_VERSION=%NAGIOSVERSION% --build-arg NRPE_VERSION=%NRPEVERSION% ^
   --build-arg PLUGIN_VERSION=%PLUGINVERSION%
 
 if ERRORLEVEL 1 (
